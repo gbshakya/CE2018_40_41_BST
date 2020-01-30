a71a8ea780aa6ebbaac1aa85d32a5623a3911733
pshreyam <52022195+pshreyam@users.noreply.github.com>
2020-01-30 09:52:38 +0545

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,12 @@
+#ifndef BST_H
+#define BST_H
+class BST
+{
+public:
+     virtual void add (int val)=0;
+     virtual void preOrderTraversal()=0;
+     virtual bool isIn (int val)=0;
+
+}
+#endif // BST_H
+
